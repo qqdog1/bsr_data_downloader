59e1350fc74fd200318aa6a8db09e345972a5201
Shawn <shawn.chou@nogle.com>
2020-01-17 19:27:45 +0800

    1. note

---
+++ src/main/java/name/qd/bsr/utils/LineNotifyUtils.java
@@ -0,0 +1,20 @@
+package name.qd.bsr.utils;
+
+public class LineNotifyUtils {
+	// https://notify-bot.line.me/doc/en/
+	// https://www.oxxostudio.tw/articles/201701/line-bot.html
+	
+	private String channelId;
+	
+	public LineNotifyUtils(String channelId) {
+		this.channelId = channelId;
+	}
+	
+	public void login() {
+		
+	}
+	
+	public void sendMessage(String message) {
+		
+	}
+}
